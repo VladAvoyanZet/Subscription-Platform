1bedfa1eeb756ed01c9c7f12b67d2b29adc3558c
VladAvoyanZet <avoyanvlad.zet@gmail.com>
2023-03-10 17:59:27 +0400

    worked in new post send logic

--- app/Services/Mail/DuplicateMailService.php
+++ app/Services/Mail/DuplicateMailService.php
@@ -7,14 +7,20 @@ use App\Models\Subscriber;
 
 class DuplicateMailService
 {
-    public function checkDuplicate($subscriberId, $postId)
+    public function checkDuplicate($subscriberId, $postId): void
     {
-        $posts = Post::find($postId);
-        $subscriber = Subscriber::find($subscriberId);
 
-        $posts->is_post_sent= 1;
-        $subscriber->is_sent = 1;
-        $posts->save();
-        $subscriber->save();
+         if ($subscriberId == 1 && $postId == 0){
+             $posts = Post::find($postId);
+             $posts->is_post_sent= 1;
+             $posts->save();
+         }else {
+             $posts = Post::find($postId);
+             $subscriber = Subscriber::find($subscriberId);
+             $posts->is_post_sent= 1;
+             $subscriber->is_sent = 1;
+             $posts->save();
+             $subscriber->save();
+         }
     }
 }
