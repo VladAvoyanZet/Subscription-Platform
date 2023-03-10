a49bac959a6e7e459f95bed29541efcf38db267b
VladAvoyanZet <avoyanvlad.zet@gmail.com>
2023-03-10 11:06:12 +0400

    chunk data from db is ready

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -2,45 +2,26 @@
 
 namespace App\Http\Controllers;
 
-
 use App\Jobs\EmailJob;
 use App\Models\Post;
 use App\Models\Site;
 use App\Models\Subscriber;
 use App\Services\Mail\CheckSentStatusService;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 use JetBrains\PhpStorm\NoReturn;
 
 class MailController extends Controller
 {
     #[NoReturn] public function index( )
     {
-        $posts = Site::with('posts')->get();
-        $subscribers = Subscriber::all()->toArray();
-        $ids = [];
-        foreach ($posts as $post) {
-            $ids[] = $post;
-        }
+        $subscribers = Subscriber::all();
+
 
         $check = new CheckSentStatusService();
 
-        foreach ($subscribers as $subscriber) {
-            foreach ($ids as $id) {
-                if ($subscriber['id'] == $id->subscriber_id && $id->posts->isNotEmpty()){
-                    foreach ($id->posts as $post) {
-                        if ($subscriber['is_sent'] == 0 && $post->is_post_sent ==  0){
-                            dispatch(new EmailJob($subscriber['email'], $id->posts, $id->domain));
-                            $check->checkStatus($subscriber['id'], $post->id);
-                        }else {
-                            dd('arden uxarkvac a');
-                        }
-
-                    }
-//                    $check->checkStatus($subscriber['is_sent']);
-//                    dd($subscriber['is_sent']);
-
-                }
-            };
-
+        foreach ($subscribers->chunk(3) as $subscriber) {
+            $check->checkStatus($subscriber);
         }
     }
 }
--- app/Services/Mail/CheckSentStatusService.php
+++ app/Services/Mail/CheckSentStatusService.php
@@ -4,20 +4,29 @@ namespace App\Services\Mail;
 
 use App\Jobs\EmailJob;
 use App\Models\Post;
+use App\Models\Site;
 use App\Models\Subscriber;
 use Illuminate\Support\Facades\Validator;
 use JetBrains\PhpStorm\NoReturn;
 
 class CheckSentStatusService
 {
-    #[NoReturn] public function checkStatus($subscriberId, $postId): void
+    #[NoReturn] public function checkStatus($subscribers): void
     {
-        $posts = Post::find($postId);
-        $subscriber = Subscriber::find($subscriberId);
+        $posts = Site::with('posts')->get();
+        $ids = [];
+        foreach ($posts as $post) {
+            $ids[] = $post;
+        };
 
-        $posts->is_post_sent= 1;
-        $subscriber->is_sent = 1;
-        $posts->save();
-        $subscriber->save();
+        foreach ($subscribers as $subscriber) {
+        foreach ($ids as $id) {
+            if ($subscriber['id'] == $id->subscriber_id && $id->posts->isNotEmpty()){
+                dd($subscriber['is_sent'],$id->posts['is_post_sent']);
+                print_r($subscriber);
+                    dispatch(new EmailJob($subscriber['email'], $id->posts, $id->domain));
+                 }
+            }
+        }
     }
 }
